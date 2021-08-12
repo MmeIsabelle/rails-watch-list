5dc4217b3f70ef5b525e923d72351735a22fe4d3
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-12 10:38:44 -0700

    Seeded 10 movies from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Cleaning de database..."
+Movie.destroy_all
+
+puts "Adding movies..."
+# Parsing
+movies = JSON.parse(URI.open('http://tmdb.lewagon.com/movie/top_rated').read)
+my_movies = movies['results'].first(10)
+
+my_movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+puts "10 movies added!"
